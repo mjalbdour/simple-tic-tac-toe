9831808c39e0c8d08e146f4e4955cfd6338cc014
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-12 01:25:01 +0300

    Stage 2: The user is the game master

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,4 +1,18 @@
 # write your code here
-print("X O X")
-print("O X O")
-print("X X O")
+def print_dashes():
+    print("---------")
+
+
+play = input()
+print_dashes()
+
+i = 0
+for j in range(3):
+    row = "| "
+    for k in range(3):
+        row = row + play[i] + " "
+        i += 1
+    row = row + "|"
+    print(row)
+
+print_dashes()
